5385c9b15ea5e6d556564cc773fa601121e46ab1
estrella-kim <puris9030@gmail.com>
2018-01-24 18:50:47 +0900

    react, react-dom install

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,18 @@ var mysql = require('mysql');
 var dbconfig = require('../config/database.js');
 var connection = mysql.createConnection(dbconfig);
 var router = express.Router();
+var react = require('react');
+var reactDOM = require('react-dom');
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
     connection.query('SELECT * from persons', function (error, results, fields) {
         if (error) throw error;
         console.log('The solution is: ', results[0]);
         res.send(results);
     });
+
     res.render('index', { title: 'Express' });
 });
 
