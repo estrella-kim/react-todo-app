3a3bc49c1c448c1e89ef6b5709456d8c2fec90dc
estrella-kim <puris9030@gmail.com>
2018-01-29 12:54:03 +0900

    set express template to html template

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'public'));
-app.set('view engine', 'hbs');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
